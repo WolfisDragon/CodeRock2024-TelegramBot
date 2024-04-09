051d8ea41fe65651e03151ce8fa7bd15d01dd47d
olegseyyy <lesha.goncharov@gmail.com>
2024-04-09 22:10:23 +0300

    0.0.1

---
+++ core/app/management/commands/runbot.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+
+import asyncio
+import logging
+
+from app.bot.main import bot
+
+class Command(BaseCommand):
+    help = "Запуск бота"
+
+    def handle(self, *args, **options):
+        try:
+            asyncio.run(bot.polling())
+        except Exception as err:
+            logging.error(f'Error: {err}')
