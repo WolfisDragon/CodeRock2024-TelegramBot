49ec4025a965bb5ad647a07dc3f144c119e2f126
WolfisDragon <mesenko.dima@yandex.ru>
2024-04-10 13:25:04 +0300

    0.0.5 migrations

--- core/app/models.py
+++ core/app/models.py
@@ -5,18 +5,25 @@ class Profile(models.Model):
   chatId = models.PositiveBigIntegerField(verbose_name = "ID пользователя", unique = True)
   username = models.CharField(max_length = 50, verbose_name = "Имя Пользователя")
 
-
   def __str__(self):
     return f'{self.chatId} {self.username}'
   
   class Meta:
     verbose_name = "Пользователь"
     verbose_name_plural = "Пользователи"
+  
+class Tag(models.Model):
+  name = models.CharField(max_length=50)
+
+  class Meta:
+    verbose_name = "Тег"
+    verbose_name_plural = "Теги"
+
 
 class Question(models.Model):
   text = models.TextField(max_length=300)
-  tagId = models.CharField(max_length=50)
-  isModerate = models.BooleanField()
+  tagId = models.ForeignKey(Tag, on_delete=models.SET_NULL, null=True)
+  isModerate = models.BooleanField(default=False)
 
   class Meta:
     verbose_name = "Вопрос"
@@ -24,8 +31,18 @@ class Question(models.Model):
 
 class Answer(models.Model):
   text = models.TextField(max_length=300)
-  questionId = models.ForeignKey(Question, on_delete=models.DO_NOTHING)
+  questionId = models.ForeignKey(Question, on_delete=models.CASCADE)
+  isModerate = models.BooleanField(default=False)
 
   class Meta:
     verbose_name = "Ответ"
     verbose_name_plural = "Ответы"
+
+class Admin(models.Model):
+  chatId = models.OneToOneField(Profile, on_delete=models.CASCADE)
+  login = models.CharField(max_length=50)
+  password = models.CharField(max_length=100)
+
+  class Meta:
+    verbose_name = "Администратор"
+    verbose_name_plural = "Администраторы"
